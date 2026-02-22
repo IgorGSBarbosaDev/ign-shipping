2bd97188b8a7531657040f8c52392235a4661f98
Igor Barbosa <igorgsbarbosadev@gmail.com>
2026-02-22 15:49:37 -0300

    initialize Spring Boot project with basic configuration and dependencies

---
+++ backend/src/test/java/br/com/ignshiopping/IgnShippingApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ignshiopping;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IgnShippingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
